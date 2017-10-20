d07a8b1a064ad5feab40cfe388b85ce5ac5f686d
Gregory Dukes <kcils360@gmail.com>
2017-10-20 15:58:28 -0700

    completed tutorial build. game runs

---
+++ AnimalTreeGame/AnimalTreeGame/Tree.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace AnimalTreeGame
+{
+    class Tree
+    {
+        Node rootNode;
+        public Tree(string question, string yesGuess, string noGuess)
+        {
+            rootNode = new Node(question);
+            rootNode.SetYesNode(new Node(yesGuess));
+            rootNode.SetNoNode(new Node(noGuess));
+        }
+        public void Query()
+        {
+            rootNode.Query();
+        }
+    }
+}
